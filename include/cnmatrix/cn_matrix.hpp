74c511daebed5d279a94c3f421d77fce1ec570cf
YuShan122 <sunnychen9262@gmail.com>
2023-03-21 11:48:02 +0000

    fix2

--- include/cnmatrix/cn_matrix.hpp
+++ include/cnmatrix/cn_matrix.hpp
@@ -2,7 +2,7 @@
 #ifndef CNMATRIX_INCLUDED_FIRST
 #error "Include <cnmatrix/cn_matrix.h>; not cn_matrix.hpp"
 #endif
-#include <cnmatrix/cn_matrix.h>
+#include "cn_matrix.h"
 #include <memory>
 
 namespace cnmatrix {
@@ -10,8 +10,8 @@ namespace cnmatrix {
         ::CnMat mat;
         std::shared_ptr<FLT> storage;
         Matrix(int rows, int cols = 1, FLT* data = 0) {
-            if(data == 0) {
-                storage = std::shared_ptr<FLT>((FLT *)calloc(rows * cols, sizeof(FLT)), free);
+            if (data == 0) {
+                storage = std::shared_ptr<FLT>((FLT*)calloc(rows * cols, sizeof(FLT)), free);
                 data = storage.get();
             }
             mat = cnMat(rows, cols, data);
@@ -19,18 +19,18 @@ namespace cnmatrix {
         static Matrix Like(const Matrix& a) {
             return Matrix(a.mat.rows, a.mat.cols);
         }
-        static Matrix Like(const CnMat & a) {
+        static Matrix Like(const CnMat& a) {
             return Matrix(a.rows, a.cols);
         }
-        operator const CnMat*() const { return &mat; }
-        operator CnMat*() { return &mat; }
-        operator const CnMat&() const { return mat; }
-        operator CnMat&() { return mat; }
-        Matrix(const CnMat& cpy) : Matrix(cpy.rows, cpy.cols){
+        operator const CnMat* () const { return &mat; }
+        operator CnMat* () { return &mat; }
+        operator const CnMat& () const { return mat; }
+        operator CnMat& () { return mat; }
+        Matrix(const CnMat& cpy): Matrix(cpy.rows, cpy.cols) {
             cnCopy(&cpy, *this, 0);
         }
-        FLT operator()(int i, int j) const { return mat(i, j);}
-        FLT& operator()(int i, int j) { return mat(i,j); }
+        FLT operator()(int i, int j) const { return mat(i, j); }
+        FLT& operator()(int i, int j) { return mat(i, j); }
         FLT operator()(int i) const { return mat(i); }
         FLT& operator()(int i) { return mat(i); }
     };
